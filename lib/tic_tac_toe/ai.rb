327e9ff9e2c26f63b205996f3d3a1a75c449297d
Apprentice <apprentice@Apprentices-MacBook-Pro.local>
2014-11-05 09:37:44 -0600

    Test AI#move() on non-empty board.

--- lib/tic_tac_toe/ai.rb
+++ lib/tic_tac_toe/ai.rb
@@ -7,7 +7,13 @@ class AI
   end
 
   def move(board)
-    board.place(token, 2)
+    return nil if board.full?
+    i = 0 + rand(9)
+    if board[i].nil?
+      board.place(token, i)
+    else
+      move(board)
+    end
   end
 
 end
--- spec/tic_tac_toe/ai_spec.rb
+++ spec/tic_tac_toe/ai_spec.rb
@@ -18,6 +18,21 @@ describe 'AI' do
       expect(board.empty?).to eq(false)
     end
 
+    it 'makes a move on a non-empty board' do
+      board.place(Game::PLAYER1_TOKEN, 0)
+      board.place(Game::PLAYER2_TOKEN, 4)
+      board.place(Game::PLAYER1_TOKEN, 2)
+
+      ai.move(board)
+
+      tokens = []
+      (0..8).each do |i|
+        tokens.push(board[i]) unless board[i].nil?
+      end
+
+      expect(tokens.count).to eq(4)
+    end
+
     xit 'blocks a win for the opposing player.' do
       board.place(:O, 0)
       board.place(:O, 1)
