d4ced7f928f0a822c59d88e3301ae5ac0c74897f
Apprentice <apprentice@Apprentices-MacBook-Pro.local>
2014-11-07 09:57:03 -0600

    Minor spacing fix.

--- spec/tic_tac_toe/human_spec.rb
+++ spec/tic_tac_toe/human_spec.rb
@@ -10,17 +10,17 @@ describe 'Human' do
   let(:human)   { Human.new(:X, console) }
 
   context '#move' do
-    it 'Displays the board' do
+    it 'displays the board' do
       expect(console).to receive(:draw_board)
       human.move(board)
     end
 
-    it 'Takes an input from the user.' do
+    it 'takes an input from the user.' do
       expect(console).to receive(:prompt)
       human.move(board)
     end
 
-    it 'Makes a move on the board' do
+    it 'makes a move on the board' do
       human.move(board)
       expect(board.empty?).to eq(false)
     end
