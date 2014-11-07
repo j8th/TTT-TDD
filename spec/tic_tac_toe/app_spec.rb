f117d357de121e08f30e1b4db2d31b3dd669a740
Apprentice <apprentice@Apprentices-MacBook-Pro.local>
2014-11-07 13:35:54 -0600

    Refactor these tests a bit.

--- spec/tic_tac_toe/app_spec.rb
+++ spec/tic_tac_toe/app_spec.rb
@@ -3,7 +3,13 @@ require 'tic_tac_toe/app'
 
 describe 'App' do
 
-  let(:app) { App.new }
+  let(:app) { 
+    app = App.new
+    # Stub Game.play() so we don't actually run the game.
+    allow(app.game).to receive(:play)
+    app
+  }
+
 
   context '#initialize' do
     it 'creates a game between a human and an ai.' do
@@ -16,10 +22,13 @@ describe 'App' do
 
   context '#run' do
     it 'runs the game' do
-      # Stub Game.play() so we don't actually run the game.
-      allow(app.game).to receive(:play)
       expect(app.game).to receive(:play)
       app.run
+    end
+
+    xit 'informs the user of the final results.' do
+      app.run
+      expect()
     end
   end
 end
